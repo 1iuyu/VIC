46a84993c4d8ca9ca1509878ee53dbad6aa3a5b5
1iuyu <145678619+1iuyu@users.noreply.github.com>
2025-05-07 16:15:17 +0800

    Update update_step_vars.c

--- vic/drivers/shared_all/src/update_step_vars.c
+++ vic/drivers/shared_all/src/update_step_vars.c
@@ -20,27 +20,21 @@ update_step_vars(all_vars_struct *all_vars,
 
     unsigned short       iveg;
     size_t               Nveg;
-    unsigned short       band;
-    size_t               Nbands;
-    veg_var_struct     **veg_var;
+    veg_var_struct      *veg_var;
 
     /* set local pointers */
     veg_var = all_vars->veg_var;
-
-    Nbands = options.SNOW_BAND;
 
     /* Set number of vegetation types */
     Nveg = veg_con[0].vegetat_type_num;
 
     /* Assign current veg characteristics */
     for (iveg = 0; iveg <= Nveg; iveg++) {
-        for (band = 0; band < Nbands; band++) {
-            veg_var[iveg][band].albedo = veg_hist[iveg].albedo[NR];
-            veg_var[iveg][band].displacement = veg_hist[iveg].displacement[NR];
-            veg_var[iveg][band].fcanopy = veg_hist[iveg].fcanopy[NR];
-            veg_var[iveg][band].LAI = veg_hist[iveg].LAI[NR];
-            veg_var[iveg][band].roughness = veg_hist[iveg].roughness[NR];
-        }
+        veg_var[iveg].albedo = veg_hist[iveg].albedo[NR];
+        veg_var[iveg].displacement = veg_hist[iveg].displacement[NR];
+        veg_var[iveg].fcanopy = veg_hist[iveg].fcanopy[NR];
+        veg_var[iveg].LAI = veg_hist[iveg].LAI[NR];
+        veg_var[iveg].roughness = veg_hist[iveg].roughness[NR];
     }
 
     return (0);
