d819ec1d666aa366a2070f9c5cbccb9a7e186a91
1iuyu <145678619+1iuyu@users.noreply.github.com>
2025-05-07 16:22:15 +0800

    Update make_energy_bal.c

--- vic/drivers/shared_all/src/make_energy_bal.c
+++ vic/drivers/shared_all/src/make_energy_bal.c
@@ -11,25 +11,19 @@
  * @brief    This routine makes an array of frozen soil data structures, one
  *           for each vegetation type and bare soil.
  *****************************************************************************/
-energy_bal_struct **
+energy_bal_struct *
 make_energy_bal(size_t nveg)
 {
-    extern option_struct options;
 
-    size_t               i, j;
-    energy_bal_struct  **temp = NULL;
+    size_t               i;
+    energy_bal_struct  *temp = NULL;
 
     temp = calloc(nveg, sizeof(*temp));
     check_alloc_status(temp, "Memory allocation error.");
 
     /** Initialize all records to unfrozen conditions */
     for (i = 0; i < nveg; i++) {
-        temp[i] = calloc(options.SNOW_BAND, sizeof(*(temp[i])));
-        check_alloc_status(temp[i], "Memory allocation error.");
-
-        for (j = 0; j < options.SNOW_BAND; j++) {
-            temp[i][j].frozen = false;
-        }
+        temp[i].frozen = false;
     }
 
     return temp;
