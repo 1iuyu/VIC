5fdc34a81d4903681f2df990adbc00ff1a3c871a
1iuyu <145678619+1iuyu@users.noreply.github.com>
2025-05-07 16:23:13 +0800

    Update make_cell_data.c

--- vic/drivers/shared_all/src/make_cell_data.c
+++ vic/drivers/shared_all/src/make_cell_data.c
@@ -11,17 +11,13 @@
  * @brief    Make an array of type cell, which contains soil column variables
  *           for a single grid cell.
  *****************************************************************************/
-cell_data_struct **
+cell_data_struct *
 make_cell_data(size_t veg_type_num)
 {
-    extern option_struct options;
 
-    size_t               i;
-    cell_data_struct   **temp;
+    cell_data_struct   *temp;
 
     temp = calloc(veg_type_num, sizeof(*temp));
-    for (i = 0; i < veg_type_num; i++) {
-        temp[i] = calloc(options.SNOW_BAND, sizeof(*(temp[i])));
-    }
+
     return temp;
 }
