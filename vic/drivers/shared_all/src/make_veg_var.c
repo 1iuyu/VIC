fc4b371a2260f42f8423782b5d97edbefcd495cf
1iuyu <145678619+1iuyu@users.noreply.github.com>
2025-05-07 16:21:10 +0800

    Update make_veg_var.c

--- vic/drivers/shared_all/src/make_veg_var.c
+++ vic/drivers/shared_all/src/make_veg_var.c
@@ -9,41 +9,36 @@
 /******************************************************************************
  * @brief    Make an array of vegitation variables for each vegitation type.
  *****************************************************************************/
-veg_var_struct **
+veg_var_struct *
 make_veg_var(size_t veg_type_num)
 {
     extern option_struct options;
 
-    size_t               i, j;
-    veg_var_struct     **temp = NULL;
+    size_t               i;
+    veg_var_struct     *temp = NULL;
 
     temp = calloc(veg_type_num, sizeof(*temp));
     check_alloc_status(temp, "Memory allocation error.");
 
-    for (i = 0; i < veg_type_num; i++) {
-        temp[i] = calloc(options.SNOW_BAND, sizeof(*(temp[i])));
-        check_alloc_status(temp[i], "Memory allocation error.");
-
-        if (options.CARBON) {
-            for (j = 0; j < options.SNOW_BAND; j++) {
-                temp[i][j].NscaleFactor = calloc(options.Ncanopy,
-                                                 sizeof(*(temp[i][j].
-                                                          NscaleFactor)));
-                check_alloc_status(temp[i][j].NscaleFactor,
-                                   "Memory allocation error.");
-                temp[i][j].aPARLayer = calloc(options.Ncanopy,
-                                              sizeof(*(temp[i][j].aPARLayer)));
-                check_alloc_status(temp[i][j].aPARLayer,
-                                   "Memory allocation error.");
-                temp[i][j].CiLayer = calloc(options.Ncanopy,
-                                            sizeof(*(temp[i][j].CiLayer)));
-                check_alloc_status(temp[i][j].CiLayer,
-                                   "Memory allocation error.");
-                temp[i][j].rsLayer = calloc(options.Ncanopy,
-                                            sizeof(*(temp[i][j].rsLayer)));
-                check_alloc_status(temp[i][j].rsLayer,
-                                   "Memory allocation error.");
-            }
+    if (options.CARBON) {
+        for (i = 0; i < veg_type_num; i++) {
+            temp[i].NscaleFactor = calloc(options.Ncanopy,
+                                          sizeof(*(temp[i].
+                                                   NscaleFactor)));
+            check_alloc_status(temp[i].NscaleFactor,
+                               "Memory allocation error.");
+            temp[i].aPARLayer = calloc(options.Ncanopy,
+                                       sizeof(*(temp[i].aPARLayer)));
+            check_alloc_status(temp[i].aPARLayer,
+                               "Memory allocation error.");
+            temp[i].CiLayer = calloc(options.Ncanopy,
+                                     sizeof(*(temp[i].CiLayer)));
+            check_alloc_status(temp[i].CiLayer,
+                               "Memory allocation error.");
+            temp[i].rsLayer = calloc(options.Ncanopy,
+                                     sizeof(*(temp[i].rsLayer)));
+            check_alloc_status(temp[i].rsLayer,
+                               "Memory allocation error.");
         }
     }
 
