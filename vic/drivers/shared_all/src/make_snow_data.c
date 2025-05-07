aa76f60e64c027be5c1ff36e7ae81e751d57d779
1iuyu <145678619+1iuyu@users.noreply.github.com>
2025-05-07 16:21:36 +0800

    Update make_snow_data.c

--- vic/drivers/shared_all/src/make_snow_data.c
+++ vic/drivers/shared_all/src/make_snow_data.c
@@ -11,22 +11,15 @@
  * @brief    Make an array of snow cover data structures, one for each
  *           vegetation type plus bare soil.
  *****************************************************************************/
-snow_data_struct **
+snow_data_struct *
 make_snow_data(size_t nveg)
 {
-    extern option_struct options;
 
-    size_t               i;
-    snow_data_struct   **temp = NULL;
+    snow_data_struct   *temp = NULL;
 
     temp = calloc(nveg, sizeof(*temp));
 
     check_alloc_status(temp, "Memory allocation error.");
-
-    for (i = 0; i < nveg; i++) {
-        temp[i] = calloc(options.SNOW_BAND, sizeof(*(temp[i])));
-        check_alloc_status(temp[i], "Memory allocation error.");
-    }
 
     return temp;
 }
